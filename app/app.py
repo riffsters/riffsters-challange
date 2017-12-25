6e671ecb51f4a1ebad5a46e40c6e2af57b9279e8
riffsters <oronriff@gmail.com>
2017-12-25 23:53:42 +0200

    Added docs and env vars to docker compose file

--- app/app.py
+++ app/app.py
@@ -1,7 +1,6 @@
 """
 App to fecth secrets from dynmodb
 """
-import os
 import json
 import yaml
 import boto3
@@ -18,10 +17,6 @@ class App(object):
         with open("app/config.yml", 'r') as ymlfile:
             cfg = yaml.load(ymlfile)
         self.client = boto3.client('dynamodb', region_name='{}'.format(cfg['aws']['region_name']))
-        # self.client = boto3.client('dynamodb', region_name='{}'.format(cfg['aws']['region_name']), \
-        # aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],\
-        # aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
-
 
     def fetch_secret(self, table_name, key):
         """
@@ -33,7 +28,8 @@ class App(object):
         #result = pprint.pprint(pretty_result)
         return secret_json
 
-    def health_check(self, container, project):
+    @staticmethod
+    def health_check(container, project):
         """
         health check function, return project info and app status
         """
---
+++ docs/SUMMARY.md
@@ -0,0 +1,6 @@
+1. Create app directory
+2. Create classes per file:
+  * router.py - contain the code for the route of the application with doc strings   
+  * app.py - contain logic class with the functions that the route executes with doc strings
+  * config.yml - contain all related configuration to avoid hardcoded docs and endpoints.
+  * app_api.py - main function file that running the application that will listen on port 5000.
